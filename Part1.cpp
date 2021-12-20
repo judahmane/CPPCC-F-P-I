fe87aae2d1f1d9ac035c04f69270753c8aae830c
Judah <67433376+judahmane@users.noreply.github.com>
2021-12-19 22:00:05 -0500

    Update Part1.cpp

--- Part1.cpp
+++ Part1.cpp
@@ -5,10 +5,18 @@
 
 int main() // entry point a function that executes when the user runs the program.
 {
-	printf("Hello, world!"); //responsible for printing Hello, world! to the console.
+	printf("Hello, wolrd!"); //responsible for printing Hello, wolrd! to the console.
 
 	return 0;//exits running succesfully. 0 = Succesful Execute. Any others mean there is a problem.
 	//determines how well a program ran
 	//Exit Codes are always intergers!
 	//IS OPTIONAL!
+	int the_answer;
+	int lucky_number = the_answer / 6; //lucky_number now equals the answer.
+
+	bool test = true; //create true/false variable
+	if (test == true) //if that variable is true which is declared in the variable.
+	{
+		std::cout << "Test = True\n"; //then execute this statement
+	}
 }
