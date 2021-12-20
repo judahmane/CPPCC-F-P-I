9a51cfed7d3f4eeeb4dc170aa2056ec8bd350d7d
Judah <67433376+judahmane@users.noreply.github.com>
2021-12-19 21:21:07 -0500

    Update Part1.cpp

--- Part1.cpp
+++ Part1.cpp
@@ -1,1 +1,14 @@
+#include <algorithm>
+#include <iostream>
+#include <string>
+#include <cstdio>
 
+int main() // entry point a function that executes when the user runs the program.
+{
+	printf("Hello, world!"); //responsible for printing Hello, world! to the console.
+
+	return 0;//exits running succesfully. 0 = Succesful Execute. Any others mean there is a problem.
+	//determines how well a program ran
+	//Exit Codes are always intergers!
+	//IS OPTIONAL!
+}
